7524f43d35426aaf89cd807176c21d2e8d9ff016
Mohit Athwani <mohit@athwani.net>
2014-02-10 20:44:35 +0530

    Basic Project set up …

---
+++ ContactBook/CBContactsCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  CBContactsCollectionViewController.h
+//  ContactBook
+//
+//  Created by Labs on 2/10/14.
+//  Copyright (c) 2014 Tera Mo Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CBContactsCollectionViewController : UICollectionViewController<UICollectionViewDataSource, UICollectionViewDelegate>
+
+@end
---
+++ ContactBook/CBContactsCollectionViewController.m
@@ -0,0 +1,69 @@
+//
+//  CBContactsCollectionViewController.m
+//  ContactBook
+//
+//  Created by Labs on 2/10/14.
+//  Copyright (c) 2014 Tera Mo Labs. All rights reserved.
+//
+
+#import "CBContactsCollectionViewController.h"
+
+@interface CBContactsCollectionViewController ()
+
+@end
+
+@implementation CBContactsCollectionViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    
+    NSArray *contactsBundleArray = [NSArray arrayWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"ContactsBundle" ofType:@"plist"]];
+    
+    
+    
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UICollectionView Datasource
+- (NSInteger)collectionView:(UICollectionView *)view numberOfItemsInSection:(NSInteger)section {
+    return 10;
+}
+
+- (NSInteger)numberOfSectionsInCollectionView: (UICollectionView *)collectionView {
+    return 1;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)cv cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    UICollectionViewCell *cell = [cv dequeueReusableCellWithReuseIdentifier:@"contactCell" forIndexPath:indexPath];
+    cell.backgroundColor = [UIColor blackColor];
+    return cell;
+}
+
+/*- (UICollectionReusableView *)collectionView:
+ (UICollectionView *)collectionView viewForSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath
+ {
+ return [[UICollectionReusableView alloc] init];
+ }*/
+@end
